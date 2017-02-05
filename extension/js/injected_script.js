270981eaaa9eeaca98866a4e63f62d0d92e27a0f
Andrea <andrea.posi@gmail.com>
2017-02-05 13:03:41 +0100

    Second commit

--- extension/js/injected_script.js
+++ extension/js/injected_script.js
@@ -1,16 +1,16 @@
 (
     function() {
-        var body = document.getElementsByTagName('body'), video = document.getElementsByTagName('video');
-        if (body.length != 1 || video.length != 1 || body[0].childNodes.length != 1)
-            return 0; //no body, no video or too much elements
+        var video = document.getElementsByTagName('video');
+        if (video.length != 1 || video[0].parentNode.childNodes.length != 1)
+            return 0; //no video or too much elements
 
         video = video[0];
-        video.style.transition = 'width 0.5s ease-in-out';
 
         if(!video.dataset.initialWidth)
         {
             video.dataset.initialWidth = video.offsetWidth;
             video.style.width = video.dataset.initialWidth + 'px';
+            video.style.transition = 'width 0.5s ease-in-out';
         }
 
         if(video.dataset.initialWidth == video.offsetWidth)
